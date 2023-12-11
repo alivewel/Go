c8ba604e88f9c54c6fd549ee936390c1bfbb2832
Aleksander <alivewel@mail.ru>
2023-12-11 15:13:17 +0300

    add main

---
+++ url-shortener/cmd/url-shortener/main.go
@@ -0,0 +1,13 @@
+package main
+
+func main() {
+	// TODO: init config: cleanenv
+
+	// TODO: init logger: slog
+
+	// TODO: init storage: sqlite
+
+	// TODO: init chi, "chi render"
+
+	// TODO: run server
+}
