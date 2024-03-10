ca5ed2dd496b99b4deb9839474a5334c24cbf4e2
Alive Wells <alivewel@pr-o4.kzn.21-school.ru>
2024-03-10 17:44:46 +0300

    add task

---
+++ concurrency_tasks/3_chan/3_def/solution/1/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"time"
+)
+
+// avoid deadlock
+func main() {
+	ch := make(chan int)
+
+	go func() {
+		select {
+		case val := <-ch:
+			fmt.Println(val)
+			// default:
+			// 	fmt.Println("default")
+		}
+	}()
+	time.Sleep(time.Second)
+	fmt.Println(runtime.NumGoroutine())
+}
+
+// решение - добавление default сценария
+
+// добавление горутины, чтобы увидеть количество активных горутин
+// при этом, если мы уберем default на 16 и 17 строке, у нас произойдет утечка горутины
+// main завершит свою работу, а созданная нами горутина нет
+
+// также можно решить задачу, используя контекст
---
+++ concurrency_tasks/3_chan/3_def/solution/2/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+// avoid deadlock
+func main() {
+	ch := make(chan int)
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+
+	go func() {
+		select {
+		case val := <-ch:
+			fmt.Println(val)
+		case <-ctx.Done():
+			fmt.Println("Done")
+		}
+	}()
+	ch <- 42
+	cancel()
+	time.Sleep(time.Second)
+}
+
+// здесь мы добавили контекст
+// и записали значение в канал
+
+// по сути можно делать что-то одно
+// отменять контекст
+// или
+// записывать значение в канал
