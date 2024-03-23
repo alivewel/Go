87998b186e473cbc425fb8e90b249923509ca203
Alive Wells <alivewel@pr-o4.kzn.21-school.ru>
2024-03-23 22:46:42 +0300

    add task

---
+++ concurrency_tasks/4_slice/9_task/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+// Remove Duplicates from Sorted Array
+func removeDuplicates(nums []int) int {
+	prevElem, len := 0, 0
+	for i, j := range nums {
+		if i == 0 {
+			prevElem = j
+			len++
+			continue
+		}
+		if j != prevElem {
+			nums[len] = j
+			len++
+			prevElem = j
+		}
+	}
+	return len
+}
+
+func main() {
+	nums := []int{1, 2, 3, 4, 4}
+	// nums := []int{}
+	k := removeDuplicates(nums)
+	fmt.Println(nums, k)
+	fmt.Println(k)
+}
