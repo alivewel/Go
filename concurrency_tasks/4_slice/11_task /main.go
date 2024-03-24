784fa250a91324bf90b4f00eab09202eb11c5a75
Alive Wells <alivewel@pr-o2.kzn.21-school.ru>
2024-03-24 18:45:36 +0300

    add ex11

--- concurrency_tasks/4_slice/11_task /main.go
+++ concurrency_tasks/4_slice/11_task /main.go
@@ -6,15 +6,11 @@ import "fmt"
 func rotate(nums []int, k int) {
 	length := len(nums)
 	k = k % length
+	fmt.Println(k)
 	newArray := make([]int, length)
-	count := 0
 	for i := range nums {
-		if i+k < length {
-			newArray[i] = nums[i+k]
-		} else {
-			newArray[i] = nums[count]
-			count++
-		}
+		newIndex := (i + k) % length
+		newArray[newIndex] = nums[i]
 	}
 	for i, j := range newArray {
 		nums[i] = j
@@ -22,12 +18,13 @@ func rotate(nums []int, k int) {
 }
 
 func main() {
+	nums := []int{1, 2, 3, 4, 5, 6, 7}
 	// nums := []int{1, 2, 3, 4, 4}
-	nums := []int{1, 2, 3, 4, 5}
+	// nums := []int{1, 2, 3, 4, 5}
 	// nums := []int{7,1,5,3,6,4}
 	// nums := []int{7,6,4,3,1}
 	// nums := []int{}
-	k := 7
+	k := 1
 	rotate(nums, k)
 	fmt.Println(nums)
 }
