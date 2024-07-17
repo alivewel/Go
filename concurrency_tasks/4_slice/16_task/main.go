7bc8bd30563d212d8d5710eff09c1e04933e7666
Alive Wells <alivewel@un-a5.kzn.21-school.ru>
2024-07-17 16:43:25 +0300

    add 16_task

--- concurrency_tasks/4_slice/16_task/main.go
+++ concurrency_tasks/4_slice/16_task/main.go
@@ -9,9 +9,16 @@ func main() {
 	test(testSlice)
 
 	// fmt.Println(testSlice) // [Hello! Hello!]
+	fmt.Println(len(testSlice))
 	fmt.Println(testSlice[:3]) // [Hello! Hello! Goodbye!]
 }
 
 func test(testSlice []string) {
 	testSlice = append(testSlice, "Goodbye!")
+	// fmt.Println(len(testSlice))
 }
+
+// слайс в функцию передается по указателю
+// данные внутри функции в слайс в итоге записываются
+// при выходе из функции длина остается прежней
+// для того, чтобы данные распечатались нужно явно передавать границы до третьего индекса массива testSlice[:3]
