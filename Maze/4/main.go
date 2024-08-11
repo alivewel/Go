07fb01e71e52f352abb1b7d7b5292159671ca4b8
Alive Wells <alivewel@un-a5.kzn.21-school.ru>
2024-08-11 20:49:15 +0300

    Maze

--- Maze/4/main.go
+++ Maze/4/main.go
@@ -13,44 +13,6 @@ type MazeWrapper struct {
 }
 
 // drawMaze выводит сгенерированный лабиринт в консоль
-// func drawMaze(maze MazeWrapper) {
-// 	width, height := maze.Cols, maze.Rows
-
-// 	for y := 0; y < height; y++ {
-// 		// Печать верхней границы текущей строки
-// 		for x := 0; x < width; x++ {
-// 			fmt.Print("+")
-// 			// if y == height-1 || maze.Vertical[y*width+x] == 1 {
-// 			if maze.Vertical[y*width+x] == 1 {
-// 				fmt.Print("---")
-// 			} else {
-// 				fmt.Print("   ")
-// 			}
-// 		}
-// 		fmt.Println("+")
-
-// 		// Печать левой границы и содержимого текущей строки
-
-// 		for x := 0; x < width; x++ {
-// 			if x == 0 || maze.Horizontal[y*width+x] == 1 {
-// 				fmt.Print("|")
-// 			} else {
-// 				fmt.Print(" ")
-// 			}
-// 			fmt.Print("   ")
-// 		}
-// 		// fmt.Println("|")
-// 		fmt.Println("")
-// 	}
-
-// 	// Печать нижней границы лабиринта
-// 	for x := 0; x < width; x++ {
-// 		fmt.Print("+---")
-// 	}
-// 	fmt.Println("+")
-// }
-
-// drawMaze выводит сгенерированный лабиринт в консоль
 func drawMaze(maze MazeWrapper) {
 	width, height := maze.Cols, maze.Rows
 
@@ -62,7 +24,6 @@ func drawMaze(maze MazeWrapper) {
 	for y := 0; y < height; y++ {
 		// Печать левой границы и содержимого текущей строки
 		for x := 0; x < width+1; x++ {
-			// if x == 0 || maze.Horizontal[y*width+x] == 1 {
 			if x == 0 || maze.Horizontal[y*width+x-1] == 1 {
 				fmt.Print("|")
 			} else {
@@ -70,14 +31,12 @@ func drawMaze(maze MazeWrapper) {
 			}
 			fmt.Print("   ")
 		}
-		// fmt.Println("=")
 		fmt.Println()
 
 		// Печать нижней границы текущей строки
-		for x := 1; x < width+1; x++ {
+		for x := 0; x < width; x++ {
 			fmt.Print("+")
-			// if y == height-1 || maze.Vertical[y*width+x-1] == 1 {
-			if y == height-1 || maze.Vertical[y*width+x] == 1 {
+			if maze.Vertical[y*width+x] == 1 {
 				fmt.Print("---")
 			} else {
 				fmt.Print("   ")
@@ -93,13 +52,13 @@ func main() {
 		Vertical: []int{1, 1, 1, 1, 1,
 			1, 1, 1, 1, 1,
 			1, 1, 1, 1, 1,
-			0, 0, 0, 0, 0,
-			0, 0, 0, 0, 0},
-		Horizontal: []int{1, 1, 1, 1, 1,
-			1, 1, 1, 1, 1,
-			1, 1, 1, 1, 1,
 			1, 1, 1, 1, 1,
 			1, 1, 1, 1, 1},
+		Horizontal: []int{0, 0, 0, 0, 1,
+			0, 0, 0, 0, 1,
+			0, 0, 0, 0, 1,
+			0, 0, 0, 0, 1,
+			0, 0, 0, 0, 1},
 		Rows: 5,
 		Cols: 5,
 	}
