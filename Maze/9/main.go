6b2c57a50556580b6eff44d6a225c22981be45c5
Alive Wells <alivewel@va-b1.kzn.21-school.ru>
2024-09-10 00:17:21 +0300

    maze final

--- Maze/9/main.go
+++ Maze/9/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"math"
+	"math/rand"
+	"time"
 )
 
 // Point представляет координаты точки в лабиринте
@@ -32,7 +34,7 @@ func (m *MazeWrapper) At(x, y int, vertical bool) int {
 		// fmt.Println("index", index, vertical)
 		return 1 // Считаем, что за пределами лабиринта есть стена
 	}
-	fmt.Println("index", index, vertical)
+	// fmt.Println("index", index, vertical)
 	if vertical {
 		return m.Vertical[index]
 	}
@@ -342,6 +344,56 @@ func printMaze2(maze MazeWrapper) {
 	fmt.Println("+")
 }
 
+func printMaze3(maze MazeWrapper, path []Point) {
+	fmt.Println("Maze visualization:")
+
+	// Создаем карту для быстрого поиска, находится ли точка на пути
+	pathMap := make(map[Point]bool)
+	for _, p := range path {
+		pathMap[p] = true
+	}
+
+	for i := 0; i < maze.Rows; i++ {
+		// Верхние стены
+		for j := 0; j < maze.Cols; j++ {
+			fmt.Print("+")
+			if i == 0 {
+				fmt.Print("-----") // Верхняя граница
+			} else {
+				if maze.At(i-1, j, false) == 1 {
+					fmt.Print("-----") // Горизонтальная стена
+				} else {
+					fmt.Print("     ")
+				}
+			}
+		}
+		fmt.Println("+")
+
+		// Вертикальные стены и пространство между ними
+		for j := 0; j < maze.Cols; j++ {
+			if j == 0 || maze.At(i, j-1, true) == 1 {
+				fmt.Print("|")
+			} else {
+				fmt.Print(" ")
+			}
+
+			// Проверяем, принадлежит ли текущая позиция пути
+			if pathMap[Point{i + 1, j + 1}] {
+				fmt.Print("  *  ") // Отображаем путь как '*'
+			} else {
+				fmt.Print("     ")
+			}
+		}
+		fmt.Println("|")
+	}
+
+	// Нижние стены
+	for j := 0; j < maze.Cols; j++ {
+		fmt.Print("+-----")
+	}
+	fmt.Println("+")
+}
+
 func main() {
 
 	// // // Создаем новый CaveWrapper с 5 строками и 5 столбцами, заполняем значением -1
@@ -362,22 +414,22 @@ func main() {
 	// cw2.Set(1, 1, 1)
 	// // cw2.Set(1, 2, 2)
 	// cw2.Print()
-	// maze := MazeWrapper{
-	// 	Vertical: []int{
-	// 		1, 1, 1, 1, 1,
-	// 		1, 1, 0, 1, 1,
-	// 		0, 1, 1, 1, 1,
-	// 		1, 0, 0, 1, 1,
-	// 		0, 0, 1, 0, 1}, // Инициализация вертикальных стен
-	// 	Horizontal: []int{
-	// 		0, 0, 0, 0, 0,
-	// 		0, 0, 0, 0, 0,
-	// 		1, 0, 0, 1, 0,
-	// 		0, 1, 0, 0, 0,
-	// 		1, 1, 1, 1, 1}, // Инициализация горизонтальных стен
-	// 	Rows:       5,
-	// 	Cols:       5,
-	// }
+	maze1 := MazeWrapper{
+		Vertical: []int{
+			1, 1, 1, 1, 1,
+			1, 1, 0, 1, 1,
+			0, 1, 1, 1, 1,
+			1, 0, 0, 1, 1,
+			0, 0, 1, 0, 1}, // Инициализация вертикальных стен
+		Horizontal: []int{
+			0, 0, 0, 0, 0,
+			0, 0, 0, 0, 0,
+			1, 0, 0, 1, 0,
+			0, 1, 0, 0, 0,
+			1, 1, 1, 1, 1}, // Инициализация горизонтальных стен
+		Rows: 5,
+		Cols: 5,
+	}
 
 	maze := MazeWrapper{
 		Vertical: []int{
@@ -397,21 +449,211 @@ func main() {
 	}
 
 	from := Point{1, 1}
-	to := Point{4, 1}
+	to := Point{2, 1}
 
 	pf := NewPathFinder(maze)
 	path := pf.Solve(maze, from, to)
 
 	fmt.Println("Path:", path)
 
+	printMaze(maze1)
+	printMaze3(maze1, path)
 	// maze2 := MazeWrapper{
 	// 	Vertical:   []int{1, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35},                         // Инициализация вертикальных стен
 	// 	Horizontal: []int{2, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135}, // Инициализация горизонтальных стен
 	// 	Rows:       5,
 	// 	Cols:       5,
 	// }
+
+	mazeGenerationSettings := MazeGenerationSettings{Rows: 10, Cols: 10}
+
+	maze3 := Generate(mazeGenerationSettings)
+
+	printMaze(maze3)
+
+	from2 := Point{1, 1}
+	to2 := Point{10, 1}
+
+	pf2 := NewPathFinder(maze3)
+	path2 := pf2.Solve(maze3, from2, to2)
+
+	printMaze3(maze3, path2)
 }
 
 // функция At - корректная
 // maze.At(i-1, j, false) - для горизонтальных стен
 // maze.At(i, j-1, true) - для вертикальных стен
+
+// MazeGenerationSettings содержит настройки для генерации лабиринта
+type MazeGenerationSettings struct {
+	Rows int
+	Cols int
+}
+
+// Generate генерирует лабиринт с использованием алгоритма Эллера
+func Generate(s MazeGenerationSettings) MazeWrapper {
+	vertical := make([]int, s.Rows*s.Cols)
+	horizontal := make([]int, s.Rows*s.Cols)
+
+	if !isGood(vertical, s.Rows, s.Cols) || !isGood(horizontal, s.Rows, s.Cols) {
+		return MazeWrapper{}
+	}
+
+	random := make([]int, s.Rows*s.Cols*3)
+	rand.Seed(time.Now().UnixNano())
+	for i := range random {
+		random[i] = rand.Intn(2)
+	}
+
+	randIndex := 0
+	counter := 1
+
+	line := make([]int, s.Cols)
+
+	merge := func(i int) {
+		mergedItem := line[i+1]
+		for col := range line {
+			if line[col] == mergedItem {
+				line[col] = line[i]
+			}
+		}
+	}
+
+	for row := 0; row < s.Rows-1; row++ {
+		for i := range line {
+			if line[i] == 0 {
+				fmt.Println("line[i]", line[i])
+				line[i] = counter
+				counter++
+				// fmt.Println("line1", line)
+				// fmt.Println("line[i]", line[i])
+			}
+		}
+		fmt.Println("---")
+		fmt.Println("line", line)
+		for col := 0; col < s.Cols-1; col++ {
+			if line[col] == line[col+1] {
+				vertical[row*s.Cols+col] = 1
+			}
+		}
+
+		for col := 0; col < s.Cols-1; col++ {
+			choice := random[randIndex]
+			randIndex++
+			if choice == 1 || line[col] == line[col+1] {
+				vertical[row*s.Cols+col] = 1
+			} else {
+				merge(col)
+			}
+		}
+		fmt.Println("row*s.Cols+s.Cols-1", row*s.Cols+s.Cols-1)
+		vertical[row*s.Cols+s.Cols-1] = 1
+
+		for col := 0; col < s.Cols; col++ {
+			choice := random[randIndex]
+			randIndex++
+			if choice == 1 {
+				count := 0
+				for c := 0; c < s.Cols; c++ {
+					if line[c] == line[col] && horizontal[row*s.Cols+c] == 0 {
+						count++
+					}
+				}
+				if count != 1 {
+					horizontal[row*s.Cols+col] = 1
+				}
+			}
+		}
+
+		for col := range line {
+			if horizontal[row*s.Cols+col] == 1 {
+				line[col] = 0
+			}
+		}
+	}
+
+	for i := range line {
+		if line[i] == 0 {
+			line[i] = counter
+			counter++
+		}
+	}
+
+	for col := 0; col < s.Cols-1; col++ {
+		choice := random[randIndex]
+		randIndex++
+		if choice == 1 || line[col] == line[col+1] {
+			vertical[(s.Rows-1)*s.Cols+col] = 1
+		} else {
+			merge(col)
+		}
+	}
+	vertical[(s.Rows-1)*s.Cols+s.Cols-1] = 1
+
+	for col := 0; col < s.Cols-1; col++ {
+		if line[col] != line[col+1] {
+			vertical[(s.Rows-1)*s.Cols+col] = 0
+			merge(col)
+		}
+		horizontal[(s.Rows-1)*s.Cols+col] = 1
+	}
+	vertical[(s.Rows-1)*s.Cols+s.Cols-1] = 1
+
+	return MazeWrapper{
+		Vertical:   vertical,
+		Horizontal: horizontal,
+		Rows:       s.Rows,
+		Cols:       s.Cols,
+	}
+}
+
+// isGood проверяет корректность данных для генерации лабиринта
+func isGood(data []int, rows, cols int) bool {
+	if rows == 0 || rows > 150 || cols == 0 || cols > 150 {
+		return false
+	}
+	if rows*cols != len(data) {
+		return false
+	}
+
+	for _, val := range data {
+		if val != 0 && val != 1 {
+			return false
+		}
+	}
+	return true
+}
+
+// drawMaze выводит сгенерированный лабиринт в консоль
+func PrintMaze(maze MazeWrapper) {
+	width, height := maze.Cols, maze.Rows
+
+	for x := 0; x < width; x++ {
+		fmt.Print("+---")
+	}
+	fmt.Println("+")
+
+	for y := 0; y < height; y++ {
+		// Печать левой границы и содержимого текущей строки
+		for x := 0; x < width+1; x++ {
+			if x == 0 || maze.Horizontal[y*width+x-1] == 1 {
+				fmt.Print("|")
+			} else {
+				fmt.Print(" ")
+			}
+			fmt.Print("   ")
+		}
+		fmt.Println()
+
+		// Печать нижней границы текущей строки
+		for x := 0; x < width; x++ {
+			fmt.Print("+")
+			if maze.Vertical[y*width+x] == 1 {
+				fmt.Print("---")
+			} else {
+				fmt.Print("   ")
+			}
+		}
+		fmt.Println("+")
+	}
+}
