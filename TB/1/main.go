4d6ff1690d79b6e5ff722c4e56e643f76b04347b
Alive Wells <alivewel@pr-o4.kzn.21-school.ru>
2024-09-10 23:30:39 +0300

    algo

--- TB/1/main.go
+++ TB/1/main.go
@@ -1,25 +1,42 @@
 package main
 
-import "fmt"
-
-func calcFee(a, b, c, d int) int {
-	if d-b > 0 {
-		return a + c*(d-b)
-	} else {
-		return a
-	}
-}
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
 
 func main() {
-	var a, b, c, d int
-	fmt.Scan(&a)
-	fmt.Scan(&b)
-	fmt.Scan(&c)
-	fmt.Scan(&d)
-	fmt.Println(calcFee(a, b, c, d))
+	// input := "1-5,6,8-9,11"
+	input := "1-5,11,6,8-9"
+	intervals := strings.Split(input, ",")
+	result := []int{}
+
+	for _, interval := range intervals {
+		if strings.Contains(interval, "-") {
+			bounds := strings.Split(interval, "-")
+			start, _ := strconv.Atoi(bounds[0])
+			end, _ := strconv.Atoi(bounds[1])
+			for i := start; i <= end; i++ {
+				result = append(result, i)
+			}
+		} else {
+			num, _ := strconv.Atoi(interval)
+			result = append(result, num)
+		}
+	}
+
+	for i, num := range result {
+		if i > 0 {
+			fmt.Print(" ")
+		}
+		fmt.Print(num)
+	}
+	fmt.Println()
 }
 
-// a = 100
-// b = 10
-// c = 12
-// d = 1
+// 1. На вход нам всегда подается отсортированный диапозон чисел. О сортировке думать не нужно.
+// 2. Можно использовать библиотеки типа strconv и strings.
+// 3. Валидировать строку скорее всего не нужно.
+// 4. Покрыть код тестами.
+// 5. Принимать диапозон чисел через fmt.Scan.
