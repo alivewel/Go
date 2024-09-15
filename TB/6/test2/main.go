a9f8fb750e4a0e3592c930b519b16c4831c418b0
Alive Wells <alivewel@mail.ru>
2024-09-15 23:28:19 +0300

    algo

--- TB/6/test2/main.go
+++ TB/6/test2/main.go
@@ -70,7 +70,7 @@ func (g *Graph) topologicalSortUtil(v int, visited []bool, stack *[]int) {
 	// }
 	*stack = append(*stack, v)
 
-	fmt.Println(*stack)
+	// fmt.Println(*stack)
 }
 
 // Функция для поиска топологической сортировки
@@ -89,14 +89,21 @@ func (g *Graph) topologicalSort() {
 
 	// Выводим содержимое стека
 	g.sortedList = stack
-	fmt.Println("Following is a Topological Sort of the given graph")
-	fmt.Println(stack)
+	// fmt.Println("Following is a Topological Sort of the given graph")
+	// fmt.Println(stack)
 }
 
 // Функция для получения времени для заданной вершины
 func (g *Graph) getTime(vertex int) int {
 	if info, exists := g.graph[vertex]; exists {
 		return info.time
+	}
+	return -1 // Возвращаем -1, если вершина не существует
+}
+
+func (g *Graph) getTotalTime(vertex int) int {
+	if info, exists := g.graph[vertex]; exists {
+		return info.totalTime
 	}
 	return -1 // Возвращаем -1, если вершина не существует
 }
@@ -110,7 +117,7 @@ func (g *Graph) getDependencies(vertex int) []int {
 
 // Функция для установки totalTime для заданной вершины
 func (g *Graph) setTotalTime(u int, totalTime int) {
-	u = u + 1 // Если вы используете смещение на +1
+	// u = u + 1 // Если вы используете смещение на +1
 	if u < g.vertices+1 {
 		// Получаем текущую информацию о графе для вершины u
 		info := g.graph[u]
@@ -123,25 +130,37 @@ func (g *Graph) setTotalTime(u int, totalTime int) {
 
 func (g *Graph) calcTotalTime() int {
 	maxValue, totalTime := 0, 0
-	for i := 1; i <= g.vertices; i++ {
+	// for i := 1; i <= g.vertices; i++ {
+	for _, i := range g.sortedList {
+		i := i
 		dependencies := g.getDependencies(i)
-		if len(dependencies) == 0 {
-			totalTime = g.getTime(i)
-			g.setTotalTime(i, totalTime)
-
-		} else {
-			for j := 1; j <= len(dependencies); j++ {
-				totalTime += g.getTime(j)
-			}
-			g.setTotalTime(i, totalTime+g.getTime(i))
+		totalTime = g.getTime(i)
+		// fmt.Println(totalTime)
+		// for j := 1; j <= len(dependencies); j++ {
+		// 	totalTime += g.getTime(j)
+		// }
+		// for j := 0; j < len(dependencies); j++ {
+		for _, j := range dependencies {
+			fmt.Println(g.getTotalTime(j))
+			totalTime += g.getTotalTime(j)
 		}
+		g.setTotalTime(i, totalTime)
+
+		fmt.Println(i, totalTime, "|", dependencies)
 		if totalTime > maxValue {
 			maxValue = totalTime
 		}
-		fmt.Println(g.getTime(i))
+		// fmt.Println(g.getTime(i))
 	}
 	g.maxTime = maxValue
 	return maxValue
+}
+
+func (g *Graph) printTotalTime() {
+	for i := 1; i <= g.vertices; i++ {
+		fmt.Println(g.getTime(i))
+	}
+	fmt.Println("maxTime", g.maxTime)
 }
 
 func main() {
@@ -153,39 +172,43 @@ func main() {
 	// g.addEdge(2, 3)
 	// g.addEdge(3, 1)
 
-	// g := NewGraph(5)
-	// g.addTime(1, 10)
-	// g.addTime(2, 5)
-	// g.addTime(3, 0)
-	// g.addTime(4, 4)
-	// g.addTime(5, 15)
-
-	// g.addEdge(1, 2)
-	// g.addEdge(1, 3)
-	// g.addEdge(1, 5)
-	// g.addEdge(2, 4)
-	// g.addEdge(5, 3)
-
-	g := NewGraph(6)
-	g.addTime(1, 2)
-	g.addTime(2, 2)
-	g.addTime(3, 15)
-	g.addTime(4, 1)
-	g.addTime(5, 2)
-	g.addTime(6, 0)
+	g := NewGraph(5)
+	g.addTime(1, 10)
+	g.addTime(2, 5)
+	g.addTime(3, 0)
+	g.addTime(4, 4)
+	g.addTime(5, 15)
 
 	g.addEdge(1, 2)
-	g.addEdge(2, 3)
-	g.addEdge(3, 4)
-	g.addEdge(4, 5)
-	g.addEdge(5, 6)
+	g.addEdge(1, 3)
+	g.addEdge(1, 5)
+	g.addEdge(2, 4)
+	g.addEdge(5, 3)
 
-	fmt.Println(g.getGraph())
+	// g := NewGraph(6)
+	// g.addTime(1, 2)
+	// g.addTime(2, 2)
+	// g.addTime(3, 15)
+	// g.addTime(4, 1)
+	// g.addTime(5, 2)
+	// g.addTime(6, 0)
+
+	// g.addEdge(1, 2)
+	// g.addEdge(2, 3)
+	// g.addEdge(3, 4)
+	// g.addEdge(4, 5)
+	// g.addEdge(5, 6)
+
+	// fmt.Println(g.getGraph())
 
 	// fmt.Println("Following is a Topological Sort of the given graph")
 	g.topologicalSort()
 
+	// g.calcTotalTime()
 	fmt.Println(g.calcTotalTime())
+
+	// fmt.Println()
+	// g.printTotalTime()
 }
 
 // добавить вместо graph    map[int][]int с map[int]struct с массивом и временем выполнения
