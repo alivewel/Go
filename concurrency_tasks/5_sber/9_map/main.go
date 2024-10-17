efc5c1d1c8c83592118406feed7fc84f078e23c5
vasilev.a178 <vasilev.a178@mail.ru>
2024-10-18 00:44:11 +0300

    add task map

---
+++ concurrency_tasks/5_sber/9_map/main.go
@@ -0,0 +1,11 @@
+package main
+
+func hash1(a string) int {
+	return math.Random()
+}
+
+func hash2(a string) int {
+	return 1
+}
+
+// какая функция для вычисления хеша лучше?
---
+++ concurrency_tasks/5_sber/9_map/solution/main.go
@@ -0,0 +1,16 @@
+package main
+
+// что выведет программа?
+
+func main() {
+	var m map[string]struct{}
+	for k := range m {
+		println(k)
+	}
+	println("end!")
+}
+
+// программа выведет end!
+// мапа пустая, мы ничего туда не положили
+
+// что такое мапа в го?
