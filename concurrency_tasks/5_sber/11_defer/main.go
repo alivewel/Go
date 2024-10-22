bf9c5fbcef613767794c47c6a742d0d0abafdeec
vasilev.a178 <vasilev.a178@mail.ru>
2024-10-22 18:25:36 +0300

    add  11_defer

---
+++ concurrency_tasks/5_sber/11_defer/main.go
@@ -0,0 +1,27 @@
+package main
+
+// Что и почему напечатает программа?
+
+func f1() int {
+	x := 1
+	defer func() {
+		x += 1
+	}()
+	return x
+}
+
+func f2() (x int) {
+	x = 1
+	defer func() {
+		x += 1
+	}()
+	return x
+}
+
+func main() {
+	println(f1())
+	println(f2())
+}
+
+// Что такое defer?
+// defer - отложенное выполнение программы
