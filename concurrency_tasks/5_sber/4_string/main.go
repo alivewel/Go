90badb46ec061f9af5b502b5fdd10699b6609366
vasilev.a178 <vasilev.a178@mail.ru>
2024-10-23 20:36:07 +0300

    add readme 4_string

--- concurrency_tasks/5_sber/4_string/main.go
+++ concurrency_tasks/5_sber/4_string/main.go
@@ -3,7 +3,7 @@ package main
 func main() {
 	str := "Привет!"
 	charCount := 0
-	// посчитать кол-во символов к строке
+	// посчитать кол-во символов в строке
 	if charCount == 7 {
 		println("Success!")
 	}
