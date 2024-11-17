a99798c59ea224aeace6c8b5435254fd297d5e2b
vasilev.a178 <vasilev.a178@mail.ru>
2024-11-17 15:39:55 +0300

    io

--- concurrency_tasks/10_io_reader_io_writer/12_write_bufio/main.go
+++ concurrency_tasks/10_io_reader_io_writer/12_write_bufio/main.go
@@ -1,19 +1,28 @@
 package main
 
 import (
+	"bufio"
 	"os"
 )
 
 func main() {
-	// f, err := os.Open("data/file.txt") // panic: write file.txt: bad file descriptor
+	// Открываем файл для чтения и записи, создаем его, если он не существует
 	f, err := os.OpenFile("data/file.txt", os.O_RDWR|os.O_CREATE, 0o644)
 	if err != nil {
 		panic(err)
 	}
 	defer f.Close() // no error handling
 
-	_, err = f.Write([]byte("Hello, World!"))
+	// Создаем новый буферизованный писатель
+	w := bufio.NewWriter(f)
+
+	// Пишем данные в буфер
+	_, err = w.Write([]byte("Hello, World!"))
 	if err != nil {
+		panic(err)
+	}
+	// Сбрасываем буфер, чтобы записать данные в файл
+	if err = w.Flush(); err != nil {
 		panic(err)
 	}
 }
