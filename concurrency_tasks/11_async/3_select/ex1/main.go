3e365272edbd2e78703335087c99feb6c94a502b
Alive Wells <alivewel@mail.ru>
2024-11-27 21:06:55 +0300

    .

---
+++ concurrency_tasks/11_async/3_select/ex1/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func longSQLQuery() chan string {
+	ch := make(chan string)
+	ch <- "str"
+	return ch
+}
+
+func main() {
+	// при 1 выполнится таймаут, при 3 - выполнится операция
+	timer := time.NewTimer(1 * time.Second)
+	select {
+	case <-timer.C:
+		fmt.Println("timer.C timeout happend")
+	case <-time.After(time.Minute):
+		// пока не выстрелит - не соберётся сборщиком мусора
+		fmt.Println("time.After timeout happend")
+	case result := <-longSQLQuery():
+		// освобождет ресурс
+		if !timer.Stop() {
+			<-timer.C
+		}
+		fmt.Println("operation result:", result)
+	}
+}
