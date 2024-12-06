f9bece6b3d1b89f995b761b702eb3e159f5d4e48
Alive Wells <alivewel@mail.ru>
2024-12-06 21:01:08 +0300

    .

--- concurrency_tasks/9_new_tasks/3_sync/2_download/main.go
+++ concurrency_tasks/9_new_tasks/3_sync/2_download/main.go
@@ -1,6 +1,6 @@
 package main
 
-import ( 
+import (
 	"errors"
 	"fmt"
 	"math/rand"
@@ -8,49 +8,72 @@ import (
 	"time"
 )
 
-// timeoutLimit - вероятность, с которой не будет возвращаться ошибка от fakeDownload(): 
+// timeoutLimit - вероятность, с которой не будет возвращаться ошибка от fakeDownload():
 // timeoutLimit = 100 - ошибок не будет;
 // timeoutLImit = 0 - всегда будет возвращаться ошибка.
-// Можете "поиграть" с этим параметром, для проверки случаев с возвращением ошибки. 
-const timeoutLimit = 90
+// Можете "поиграть" с этим параметром, для проверки случаев с возвращением ошибки.
+const timeoutLimit = 0
 
-type Result struct { 
+type Result struct {
 	msg string
 	err error
 }
 
-
-// fakeDownload - имитирует разное время скачивания для разных адресов 
+// fakeDownload - имитирует разное время скачивания для разных адресов
 func fakeDownload(url string) Result {
 	r := rand.Intn(100)
-	time.Sleep(time.Duration(r) * time.Millisecond) 
+	time.Sleep(time.Duration(r) * time.Millisecond)
 	if r > timeoutLimit {
 		return Result{
 			err: errors.New(fmt.Sprintf("failed to download data from %s: timeout", url)),
-		} 
+		}
 	}
 
 	return Result{
-	msg: fmt.Sprintf("downloaded data from %s\n", url),
-	} 
+		msg: fmt.Sprintf("downloaded data from %s\n", url),
+	}
 }
 
-// download - параллельно скачивает данные из urls 
+// download - параллельно скачивает данные из urls
 func download(urls []string) ([]string, error) {
+	wg := sync.WaitGroup{}
+	ch := make(chan string)
+	res := make([]string, 0)
+	for _, url := range urls {
+		wg.Add(1)
+		go func(url string) {
+			defer wg.Done()
+			res := fakeDownload(url)
+			if res.err == nil {
+				ch <- res.msg
+			} else {
+				fmt.Println(res.err)
+			}
+		}(url)
+	}
 
-	return nil, nil
+	go func(chan string) {
+		wg.Wait()
+		close(ch)
+	}(ch)
+
+	for in := range ch {
+		// fmt.Println(in)
+		res = append(res, in)
+
+	}
+	return res, nil
 }
 
- 
 func main() {
 	msgs, err := download([]string{
-	"https://example.com/e25e26d3-6aa3-4d79-9ab4-fc9b71103a8c.xml", 
-	"https://example.com/a601590e-31c1-424a-8ccc-decf5b35c0f6.xml", 
-	"https://example.com/1cf0dd69-a3e5-4682-84e3-dfe22ca771f4.xml", 
-	"https://example.com/ceb566f2-a234-4cb8-9466-4a26f1363aa8.xml", 
-	"https://example.com/b6ed16d7-cb3d-4cba-b81a-01a789d3a914.xml",
+		"https://example.com/e25e26d3-6aa3-4d79-9ab4-fc9b71103a8c.xml",
+		"https://example.com/a601590e-31c1-424a-8ccc-decf5b35c0f6.xml",
+		"https://example.com/1cf0dd69-a3e5-4682-84e3-dfe22ca771f4.xml",
+		"https://example.com/ceb566f2-a234-4cb8-9466-4a26f1363aa8.xml",
+		"https://example.com/b6ed16d7-cb3d-4cba-b81a-01a789d3a914.xml",
 	})
-	if err != nil { 
+	if err != nil {
 		panic(err)
 	}
 	fmt.Println(msgs)
