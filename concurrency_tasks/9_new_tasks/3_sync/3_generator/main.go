4b6e04b902e9504618b47c881912240959a12079
Alive Wells <alivewel@mail.ru>
2024-12-06 23:01:59 +0300

    .

---
+++ concurrency_tasks/9_new_tasks/3_sync/3_generator/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"context"
+	"fmt"
+)
+
+func main() {
+	ctx := context.Background()
+	pipeline := squarer(ctx, generator(ctx, 1, 2, 3))
+	for x := range pipeline {
+		fmt.Println(x)
+	}
+}
+
+func generator(ctx context.Context, in ...int) <-chan int {
+	return nil
+}
+
+func squarer(ctx context.Context, in <-chan int) <-chan int {
+	return nil
+}
