78f394b4e5f2ee62f6ea9fab2c07dcf0fafcac3d
Alive Wells <alivewel@mail.ru>
2024-12-07 18:34:20 +0300

    .

--- concurrency_tasks/9_new_tasks/3_sync/4_repeat_fn/main.go
+++ concurrency_tasks/9_new_tasks/3_sync/4_repeat_fn/main.go
@@ -2,17 +2,45 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"math/rand"
 )
 
+// Функция repeatFn бесконечно вызывает функцию fn и пишет результат ее работы в возвращаемый канал.
+// Прекращает работу раньше, если контекст отменен.
+
+// Функция take читает не более чем num из канала in, пока in открыт, и пишет значение в возвращаемый канал.
+// Прекращает работу раньше, если контекст отменен.
+
 func repeatFn(ctx context.Context, fn func() interface{}) <-chan interface{} {
-	// напишите ваш код здесь
-	return nil
+	ch := make(chan interface{})
+	go func(chan interface{}) {
+		defer close(ch)
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case ch <- fn():
+			}
+		}
+	}(ch)
+	return ch
 }
 
 func take(ctx context.Context, in <-chan interface{}, num int) <-chan interface{} {
-	// напишите ваш код здесь
-	return nil
+	ch := make(chan interface{})
+	go func(chan interface{}) {
+		defer close(ch)
+		for i := 0; i < num; i++ {
+			select {
+			case <-ctx.Done():
+				return
+			case ch <- in:
+			}
+		}
+		// close(ch)
+	}(ch)
+	return ch
 }
 
 func main() {
@@ -25,6 +53,7 @@ func main() {
 	for num := range take(ctx, repeatFn(ctx, rand), 3) {
 		res = append(res, num)
 	}
+	fmt.Println(res)
 	if len(res) != 3 {
 		panic("wrong code")
 	}
