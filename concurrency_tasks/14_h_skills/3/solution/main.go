c65fb1421c707464635520166e7c72b72a339439
Alive Wells <alivewel@mail.ru>
2024-12-13 15:53:29 +0300

    .

--- concurrency_tasks/14_h_skills/3/solution/main.go
+++ concurrency_tasks/14_h_skills/3/solution/main.go
@@ -8,15 +8,15 @@ import (
 
 func main() {
 	slice2 := []int{1, 2, 3, 4, 5}
-	fmt.Println(len(slice2), cap(slice2))
+	fmt.Println(len(slice2), cap(slice2)) // 5 5
 
 	slice3 := slice2[1:]
-	fmt.Println(slice3, len(slice3), cap(slice3))
+	fmt.Println(slice3, len(slice3), cap(slice3)) // [2, 3, 4, 5] 4 4
 
 	slice4 := slice2[:3]
-	fmt.Println(slice4, len(slice4), cap(slice4))
+	fmt.Println(slice4, len(slice4), cap(slice4)) // [1, 2, 3] 3 5
 
 	slice4 = append(slice4, 1, 2)
-	fmt.Println(slice4)
-	fmt.Println(slice2)
+	fmt.Println(slice4) // [1, 2, 3, 1, 2]
+	fmt.Println(slice2) // [1, 2, 3, 1, 2]
 }
