75d481c1f353a6bceebaf6353c83da9d64a2ce8a
vasilev.a178 <vasilev.a178@mail.ru>
2025-01-16 14:41:20 +0300

    .

---
+++ concurrency_tasks/6_leetcode/19_remove-nth-node-from-end-of-list/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func (it *ListNode) String() (result string) {
+	for cur := it; cur != nil; cur = cur.Next {
+		if cur == cur.Next {
+			return "CYCLE DETECTED"
+		}
+		result += strconv.Itoa(cur.Val) + " "
+	}
+	return result
+}
+
+func main() {
+	// Создание первого списка [1, 2, 3, 4, 5]
+	list := &ListNode{1,
+		&ListNode{2,
+			&ListNode{3,
+				&ListNode{4,
+					&ListNode{5, nil}}}}}
+
+	// Создание второго списка [1, 2, 3, 5]
+	res := &ListNode{1,
+		&ListNode{2,
+			&ListNode{3,
+				&ListNode{5, nil}}}}
+
+	fmt.Println(removeNthFromEnd(list, 2)) // Список: 8->9->9->9->0->0->0->1 Число: 10009998
+	fmt.Println(res)
+}
+
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+	dummy := head
+
+	length := 0
+	cur := head
+	for cur != nil {
+		length++
+		cur = cur.Next
+	}
+	cur = dummy
+	for i := 0; i < length-n-1; i++ {
+		cur = cur.Next
+	}
+
+    if length == n {
+        dummy = dummy.Next
+    } else {
+       cur.Next = cur.Next.Next
+    }
+
+	return dummy
+}
