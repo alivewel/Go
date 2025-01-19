90175becd8a232cae9e71215b2818fa3702f0c5e
vasilev.a178 <vasilev.a178@mail.ru>
2025-01-19 19:24:24 +0300

    .

---
+++ concurrency_tasks/6_leetcode/145_binary-tree-postorder-traversal/main.go
@@ -0,0 +1,45 @@
+package main
+
+import "fmt"
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func createTree(nodes []interface{}, index int) *TreeNode {
+	if index >= len(nodes) || nodes[index] == nil {
+		return nil
+	}
+
+	node := &TreeNode{Val: nodes[index].(int)}
+	node.Left = createTree(nodes, 2*index+1)
+	node.Right = createTree(nodes, 2*index+2)
+	return node
+}
+
+func main() {
+	// nodes := []interface{}{1, 2, 3, 4, 5, nil, 8, nil, nil, 6, 7, 9}
+	nodes := []interface{}{1, 2, 3, 4, 5, 6, 7}
+	root := createTree(nodes, 0)
+
+	// Call the preorderTraversal function
+	result := postorderTraversal(root)
+	fmt.Println(result) // Output the result
+}
+
+func postorderTraversal(root *TreeNode) []int {
+	result := []int{}
+	traverse(root, &result)
+	return result
+}
+
+func traverse(root *TreeNode, result *[]int) {
+	if root == nil {
+		return
+	}
+	traverse(root.Left, result)
+	traverse(root.Right, result)
+	*result = append(*result, root.Val)
+}
