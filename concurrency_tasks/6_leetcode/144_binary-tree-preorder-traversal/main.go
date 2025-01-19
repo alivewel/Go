a3c173803c5b55d075eeca1be47ecff603a93ee0
vasilev.a178 <vasilev.a178@mail.ru>
2025-01-19 19:17:45 +0300

    .

--- concurrency_tasks/6_leetcode/144_binary-tree-preorder-traversal/main.go
+++ concurrency_tasks/6_leetcode/144_binary-tree-preorder-traversal/main.go
@@ -1,16 +1,45 @@
 package main
 
+import "fmt"
+
 type TreeNode struct {
-    Val int
-    Left *TreeNode
-    Right *TreeNode
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func createTree(nodes []interface{}, index int) *TreeNode {
+	if index >= len(nodes) || nodes[index] == nil {
+		return nil
+	}
+
+	node := &TreeNode{Val: nodes[index].(int)}
+	node.Left = createTree(nodes, 2*index+1)
+	node.Right = createTree(nodes, 2*index+2)
+	return node
 }
 
 func main() {
+	// Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
+	nodes := []interface{}{1, 2, 3, 4, 5, nil, 8, nil, nil, 6, 7, 9}
+	root := createTree(nodes, 0)
 
+	// Call the preorderTraversal function
+	result := preorderTraversal(root)
+	fmt.Println(result) // Output the result
 }
 
 func preorderTraversal(root *TreeNode) []int {
-	
-	return nil
+	result := []int{}
+	traverse(root, &result)
+	return result
+}
+
+func traverse(root *TreeNode, result *[]int) {
+	if root == nil {
+		return
+	}
+	*result = append(*result, root.Val)
+	traverse(root.Left, result)
+	traverse(root.Right, result)
 }
