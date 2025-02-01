cef50a806ecbf47d26b8a8568e813485333d5665
vasilev.a178 <vasilev.a178@mail.ru>
2025-02-01 19:54:28 +0300

    .

--- concurrency_tasks/6_leetcode/other/200_number-of-islands/main.go
+++ concurrency_tasks/6_leetcode/other/200_number-of-islands/main.go
@@ -6,28 +6,42 @@ func numIslands(grid [][]byte) int {
 	counter := int(0)
 	for i := 0; i < len(grid); i++ {
 		for j := 0; j < len(grid[i]); j++ {
-			if i > 0 && j > 0 && grid[i-1][j] == '0' && grid[i][j-1] == '0'  && grid[i][j] == '1' {
+			// Случай для верхнего левого угла (i == 0 && j == 0)
+			if i == 0 && j == 0 && grid[i][j] == '1' {
 				counter++
-				// fmt.Print(i, j)
 				fmt.Println(i, j)
 			}
-			if i == 0 && j == 0 && grid[i-1][j] == '0' && grid[i][j-1] == '0'  && grid[i][j] == '1' {
+			// Случай для первой строки (i == 0)
+			if i == 0 && j > 0 && grid[i][j-1] == '0' && grid[i][j] == '1' {
 				counter++
-				// fmt.Print(i, j)
 				fmt.Println(i, j)
 			}
-			// fmt.Print(grid[i][j], " ")
+			// Случай для первого столбца (j == 0)
+			if j == 0 && i > 0 && grid[i-1][j] == '0' && grid[i][j] == '1' {
+				counter++
+				fmt.Println(i, j)
+			}
+			// Общий случай для остальных ячеек
+			if i > 0 && j > 0 && grid[i-1][j] == '0' && grid[i][j-1] == '0' && grid[i][j] == '1' {
+				counter++
+				fmt.Println(i, j)
+			}
 		}
-		fmt.Println()
 	}
     return counter
 }
 
 func main() {
+	// grid := [][]byte{
+    //     {'1', '1', '1', '1', '0'},
+    //     {'1', '1', '0', '1', '0'},
+    //     {'1', '1', '0', '0', '0'},
+    //     {'0', '0', '0', '0', '0'},
+    // }
 	grid := [][]byte{
-        {'1', '1', '1', '1', '0'},
-        {'1', '1', '0', '1', '0'},
-        {'1', '1', '0', '0', '0'},
+        {'1', '0', '1', '0', '1'},
+        {'1', '0', '1', '0', '1'},
+        {'1', '1', '1', '1', '1'},
         {'0', '0', '0', '0', '0'},
     }
 	res := numIslands(grid)
