b125f13fc1e40b6dd94cfd9636c8ff29d5ece156
vasilev.a178 <vasilev.a178@mail.ru>
2025-02-01 22:03:54 +0300

    .

--- concurrency_tasks/6_leetcode/other/350_intersection-of-two-arrays-ii/main.go
+++ concurrency_tasks/6_leetcode/other/350_intersection-of-two-arrays-ii/main.go
@@ -1,9 +1,26 @@
 package main
 
+import "fmt"
+
 func intersect(nums1 []int, nums2 []int) []int {
-	return nil
+	res := []int{}
+	p1, p2 := 0, 0
+	for p1 < len(nums1) && p2 < len(nums2) {
+		if nums1[p1] < nums2[p2] {
+			p1++
+		} else if nums1[p1] > nums2[p2] {
+			p2++
+		} else {
+			res = append(res, nums1[p1]) // не путать последовательность
+			p1++ // сначала добавить, потом сдвинуть указатели
+			p2++
+		}
+	}
+	return res
 }
 
 func main() {
-
+	nums1 := []int{-3, 2, 2, 5, 8, 19, 31}
+	nums2 := []int{1, 2, 2, 2, 6, 19, 52}
+	fmt.Println(intersect(nums1, nums2))
 }
