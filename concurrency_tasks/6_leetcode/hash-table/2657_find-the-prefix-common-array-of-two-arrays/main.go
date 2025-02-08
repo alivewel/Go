be058ab1fdda10992ce6d2affac9987262c902a0
vasilev.a178 <vasilev.a178@mail.ru>
2025-02-09 00:21:38 +0300

    .

--- concurrency_tasks/6_leetcode/hash-table/2657_find-the-prefix-common-array-of-two-arrays/main.go
+++ concurrency_tasks/6_leetcode/hash-table/2657_find-the-prefix-common-array-of-two-arrays/main.go
@@ -1,10 +1,32 @@
 package main
 
-func findThePrefixCommonArray(A []int, B []int) []int {
+import "fmt"
 
-	return nil
+func findThePrefixCommonArray(A []int, B []int) []int {
+	frequency := make([]int, len(A)+1)
+	result := make([]int, len(A))
+	counter := 0
+	for i := range A {
+		frequency[A[i]]++
+		frequency[B[i]]++
+		if A[i] == B[i] {
+			counter++
+			result[i] = counter
+			continue
+		}
+		if frequency[A[i]] == 2 {
+			counter++
+		}
+		if frequency[B[i]] == 2 {
+			counter++
+		}
+		result[i] = counter
+	}
+	return result
 }
 
 func main() {
-
+	A := []int{1,3,2,4}
+	B := []int{3,1,2,4}
+	fmt.Println(findThePrefixCommonArray(A, B))
 }
