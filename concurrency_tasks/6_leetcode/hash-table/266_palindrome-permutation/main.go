4abc3e70a4166f48995af68367ee87fde118ba03
vasilev.a178 <vasilev.a178@mail.ru>
2025-02-11 16:38:31 +0300

    .

--- concurrency_tasks/6_leetcode/hash-table/266_palindrome-permutation/main.go
+++ concurrency_tasks/6_leetcode/hash-table/266_palindrome-permutation/main.go
@@ -1,10 +1,28 @@
 package main
 
-func canPermutePalindrome() bool {
+import "fmt"
 
-	return false
+func canPermutePalindrome(str string) bool {
+	countSort := make([]int, 26)
+	for _, s := range str {
+		countSort[s-'a']++
+	}
+	cnt := 0
+	for _, count := range countSort {
+		if count % 2 != 0 {
+			cnt++
+		}
+		if cnt > 1 {
+			return false
+		}
+	}
+	return true
 }
 
 func main() {
+	str1 := "carerac" // true
+	str2 := "code"    // false
 
+	fmt.Println(canPermutePalindrome(str1))
+	fmt.Println(canPermutePalindrome(str2))
 }
