c817620b51f4316742ede821eed31dcc4defaed1
vasilev.a178 <vasilev.a178@mail.ru>
2025-02-12 16:32:30 +0300

    .

--- concurrency_tasks/6_leetcode/prefix-sum/189_rotate-array/2/main.go
+++ concurrency_tasks/6_leetcode/prefix-sum/189_rotate-array/2/main.go
@@ -4,31 +4,24 @@ import "fmt"
 
 // Rotate Array
 func rotate(nums []int, k int) {
-	if k < 0 {
-		k = len(nums) + k
-	}
-	length := len(nums)
-	k = k % length
-	fmt.Println(k)
-	newArray := make([]int, length)
-	for i := range nums {
-		newIndex := (i + k) % length
-		fmt.Println(i, newIndex)
-		newArray[newIndex] = nums[i]
-	}
-	for i, j := range newArray {
-		nums[i] = j
+	n := len(nums)
+	k = k % n
+	rotateSubArray(nums, 0, n-1) // подумать
+	rotateSubArray(nums, 0, k-1)
+	rotateSubArray(nums, k, n-1)
+}
+
+func rotateSubArray(nums []int, l, r int) {
+	for l < r { // подумать
+		nums[l], nums[r] = nums[r], nums[l]
+		l++
+		r--
 	}
 }
 
 func main() {
-	nums := []int{1, 2, 3, 4, 5, 6, 7}
-	// nums := []int{1, 2, 3, 4, 4}
-	// nums := []int{1, 2, 3, 4, 5}
-	// nums := []int{7,1,5,3,6,4}
-	// nums := []int{7,6,4,3,1}
-	// nums := []int{}
-	k := -6
+	nums := []int{1, 2, 3, 4, 5, 6}
+	k := 3
 	rotate(nums, k)
 	fmt.Println(nums)
 }
