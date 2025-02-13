cb682176ca3df46e5134f26e9fcab63367d6b7a0
vasilev.a178 <vasilev.a178@mail.ru>
2025-02-13 17:20:40 +0300

    .

---
+++ concurrency_tasks/6_leetcode/prefix-sum/1974_minimum-difference-between-highest-and-lowest-of-k-scores/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func minimumDifference(nums []int, k int) int {
+	
+    return 0
+}
+
+func main() {
+	nums := []int{1, 3, 5, 4, 7}
+	fmt.Println(minimumDifference(nums, 1))
+}
