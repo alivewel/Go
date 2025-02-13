d6c90693c8bacb53bed5860a6c73056f98dcf9dd
vasilev.a178 <vasilev.a178@mail.ru>
2025-02-14 00:09:24 +0300

    .

--- concurrency_tasks/6_leetcode/prefix-sum/1464_maximum-product-of-two-elements-in-an-array/main.go
+++ concurrency_tasks/6_leetcode/prefix-sum/1464_maximum-product-of-two-elements-in-an-array/main.go
@@ -3,7 +3,20 @@ package main
 import "fmt"
 
 func maxProduct(nums []int) int {
-	return 0
+	maxVal1, maxVal2 := 0, 0
+	for _, num := range nums {
+		if num > maxVal1 {
+			if maxVal2 < maxVal1 {
+				maxVal2 = maxVal1
+			}
+			maxVal1 = num
+			continue
+		}
+		if num > maxVal2 {
+			maxVal2 = num
+		}
+	}
+	return (maxVal1 - 1) * (maxVal2 - 1)
 }
 
 func main() {
