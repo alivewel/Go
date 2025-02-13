4e7d621b69a8e44800196683aac1a6aa93ad3297
vasilev.a178 <vasilev.a178@mail.ru>
2025-02-13 23:26:47 +0300

    .

--- concurrency_tasks/6_leetcode/prefix-sum/1974_minimum-difference-between-highest-and-lowest-of-k-scores/main.go
+++ concurrency_tasks/6_leetcode/prefix-sum/1974_minimum-difference-between-highest-and-lowest-of-k-scores/main.go
@@ -1,13 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"sort"
+)
 
 func minimumDifference(nums []int, k int) int {
-	
-    return 0
+	sort.Ints(nums)
+	result := math.MaxInt
+	for i := range nums {
+		l := i
+		r := i + k - 1
+		curResult := nums[r] - nums[l]
+		if curResult < result {
+			result = curResult
+		}
+		if i == len(nums) - k {
+			break
+		}
+	}
+	return result
 }
 
 func main() {
 	nums := []int{1, 3, 5, 4, 7}
-	fmt.Println(minimumDifference(nums, 1))
+	fmt.Println(minimumDifference(nums, 2))
 }
