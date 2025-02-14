2ca0db26458ca164282191c9dc7f1204c4ee7e6d
vasilev.a178 <vasilev.a178@mail.ru>
2025-02-14 14:16:28 +0300

    .

--- concurrency_tasks/6_leetcode/two_pointers/977_squares-of-a-sorted-array/main.go
+++ concurrency_tasks/6_leetcode/two_pointers/977_squares-of-a-sorted-array/main.go
@@ -1,2 +1,28 @@
+package main
 
-fd
+import (
+	"fmt"
+	"math"
+)
+
+func sortedSquares(nums []int) []int {
+	result := make([]int, len(nums))
+	l, r, index := 0, len(nums) - 1, len(nums) - 1
+	for l <= r {
+		if nums[l]*nums[l] > nums[r]*nums[r] {
+			result[index] = nums[l] * nums[l]
+			l++
+		} else {
+			result[index] = nums[r] * nums[r]
+			r--
+		}	
+		index--
+	} 
+	
+	return result
+}
+
+func main() {
+	nums := []int{-3, -2, 0, 1, 3, 5}
+	fmt.Println(sortedSquares(nums))
+}
