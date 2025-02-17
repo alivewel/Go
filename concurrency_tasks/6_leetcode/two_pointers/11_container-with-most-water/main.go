d2168d2906b6375b90fa987316742582af2630d9
vasilev.a178 <vasilev.a178@mail.ru>
2025-02-17 09:42:22 +0300

    .

--- concurrency_tasks/6_leetcode/two_pointers/11_container-with-most-water/main.go
+++ concurrency_tasks/6_leetcode/two_pointers/11_container-with-most-water/main.go
@@ -8,14 +8,8 @@ func maxArea(height []int) int {
     l, r := 0, len(height) - 1
 	maxArea := 0
 	for l < r { // l <= r с таким условием тоже работает
-		minHeight := height[l]
-		if height[l] > height[r] {
-			minHeight = height[r]
-		}
-		curArea := (r - l) * minHeight
-		if curArea > maxArea {
-			maxArea = curArea
-		}
+		curArea := (r - l) * min(height[l], height[r])
+		maxArea = max(curArea, maxArea)
 		if height[l] > height[r] {
 			r--
 		} else {
@@ -29,3 +23,5 @@ func main() {
 	height := []int{1, 8, 6, 2, 5, 4, 8, 3, 7}
 	fmt.Println(maxArea(height))
 }
+
+// оказывается можно не реализовывать функции max и min, они сами подхватываются из стандартной библиотеки
