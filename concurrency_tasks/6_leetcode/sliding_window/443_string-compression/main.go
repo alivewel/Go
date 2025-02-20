05854dfa139ae1611e66e33407f8f6302971db7d
vasilev.a178 <vasilev.a178@mail.ru>
2025-02-20 13:59:15 +0300

    .

---
+++ concurrency_tasks/6_leetcode/sliding_window/443_string-compression/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func compress(chars []byte) int {
+	for
+    return 0
+}
+
+func main() {
+	chars := []byte{"a","a","b","b","c","c","c"}
+	fmt.Println(compress(chars))
+}
