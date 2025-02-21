41918c8ecc19bc146d7a8da78a2f3388dcac3962
vasilev.a178 <vasilev.a178@mail.ru>
2025-02-21 23:30:45 +0300

    .

--- concurrency_tasks/6_leetcode/sliding_window/overlapping_windows/487_max-consecutive-ones-ii/diary.md
+++ concurrency_tasks/6_leetcode/sliding_window/overlapping_windows/487_max-consecutive-ones-ii/diary.md
@@ -41,3 +41,41 @@ for r+1 < len(nums) && (nums[r] == nums[r+1] || countZeros < 1)
 
 Инициализация zeroIdx := -1:
 Нужна для обработки случаев, когда в окне нет нулей, чтобы избежать ошибок при попытке использовать индекс последнего нуля.
+
+4) Все косяки расписал в комментариях.
+
+``` go
+func longestStockGrowth(stock []int) int {
+	l, r := 0, 0
+	result := 0
+	countZero, idxZero := 0, -1
+	if stock[0] == 0 {
+		countZero = 1
+		idxZero = 0
+	}
+	// for r < len(stock) { // 1 косяк
+	for l < len(stock) {
+		// for r+1 < len(stock) && (stock[r] == stock[r+1] || countZero < 1) { // 2 косяк 
+		for r+1 < len(stock) && (stock[r+1] == 1 || countZero < 1) { 
+			if stock[r+1] == 0 {
+				countZero++
+				idxZero = r + 1
+			}
+			r++
+		}
+		curCountZero := r - l + 1
+		result = max(result, curCountZero)
+		countZero = 0
+		// l = r + 1 // 3 косяк 
+		// r = r + 1 // 4 косяк 
+		if idxZero != -1 {
+			// l = max(r + 1, idxZero) // 5 косяк 
+			l = max(l + 1, idxZero+1)
+		} else {
+			// l = r + 1 // чтобы не зациклиться // 6 косяк 
+			l = l + 1
+		}
+	}
+    return result 
+}
+```
--- concurrency_tasks/6_leetcode/sliding_window/overlapping_windows/487_max-consecutive-ones-ii/main.go
+++ concurrency_tasks/6_leetcode/sliding_window/overlapping_windows/487_max-consecutive-ones-ii/main.go
@@ -2,35 +2,88 @@ package main
 
 import "fmt"
 
-func longestStockGrowth(nums []int) int {
+func longestStockGrowth(stock []int) int {
 	l, r := 0, 0
 	result := 0
-	countZeros := 0
-	for r < len(nums) {
-		for r+1 < len(nums) && (nums[r] == nums[r+1] || countZeros < 1) { 
-			if nums[r] == 0 {
-				countZeros++
+	countZero, idxZero := 0, -1
+	if stock[0] == 0 {
+		countZero = 1
+		idxZero = 0
+	}
+	// for r < len(stock) { // 1 косяк
+	for l < len(stock) {
+		// for r+1 < len(stock) && (stock[r] == stock[r+1] || countZero < 1) { // 2 косяк 
+		for r+1 < len(stock) && (stock[r+1] == 1 || countZero < 1) { 
+			if stock[r+1] == 0 {
+				countZero++
+				idxZero = r + 1
 			}
 			r++
 		}
-		fmt.Println(l, r)
-		if nums[l] != 0 {
-			curCountZero := r - l + 1
-			result = max(result, curCountZero)
-			countZeros = 0
+		curCountZero := r - l + 1
+		result = max(result, curCountZero)
+		countZero = 0
+		// l = r + 1 // 3 косяк 
+		// r = r + 1 // 4 косяк 
+		if idxZero != -1 {
+			// l = max(r + 1, idxZero) // 5 косяк 
+			l = max(l + 1, idxZero+1)
+		} else {
+			// l = r + 1 // чтобы не зациклиться // 6 косяк 
+			l = l + 1
 		}
-		l = r + 1
-		r = r + 1
 	}
     return result 
 }
 
+// func longestStockGrowth(stock []int) int {
+//     l := 0 // Левый указатель окна
+//     r := 0 // Правый указатель окна
+//     result := 0 // Результирующая максимальная длина
+//     zerosCount := 0 // Количество нулей в текущем окне
+//     zeroIdx := -1 // Индекс последнего нуля в окне
+
+//     if len(stock) > 0 && stock[0] == 0 {
+//         zerosCount = 1
+//         zeroIdx = 0
+//     }
+
+//     for l < len(stock) {
+//         // Расширяем окно, пока следующий элемент 1 или количество нулей меньше 1
+//         for r+1 < len(stock) && (stock[r+1] == 1 || zerosCount < 1) {
+//             if stock[r+1] == 0 {
+//                 zerosCount += 1
+//                 zeroIdx = r + 1
+//             }
+//             r += 1
+//         }
+
+//         // обновляем ответ
+//         windowSize := r - l + 1
+//         if windowSize > result {
+//             result = windowSize
+//         }
+
+//         // сдвигаем на zeroIdx когда у нас есть 0 в окне
+//         // сдвигаем на l + 1 если нулей в окне нет (нужно чтобы не зациклиться)
+//         zerosCount = 0
+//         if zeroIdx != -1 {
+//             l = max(zeroIdx+1, l+1)
+//         } else {
+//             l = l + 1
+//         }
+//     }
+
+//     return result
+// }
+
+
 func main() {
-	// nums := []int{1,1,0,1,1,1}
-	nums := []int{0, 0, 0, 0}
-	// nums := []int{0, 1, 1, 1, 0, 1, 1}
-	// nums := []int{0, 1, 0, 1, 0, 1, 0}
-	fmt.Println(longestStockGrowth(nums))
+	// stock := []int{1,1,0,1,1,1}
+	stock := []int{0, 0, 0, 0}
+	// stock := []int{0, 1, 1, 1, 0, 1, 1}
+	// stock := []int{0, 1, 0, 1, 0, 1, 0}
+	fmt.Println(longestStockGrowth(stock))
 }
 
 // func runTests() {
