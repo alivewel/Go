444a05527c4ca37682d368ac12c845525af1c2f1
vasilev.a178 <vasilev.a178@mail.ru>
2025-02-24 11:28:50 +0300

    .

---
+++ concurrency_tasks/6_leetcode/hash-table/15_3sum/1/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+// решение с помощью двух указателей
+func threeSum(nums []int) [][]int {
+    // отсортировать массив
+	sort.Ints(nums)
+	re
+}
+
+func main() {
+	nums := []int{2, 7, 11, 15}
+	target := 20
+	fmt.Println(threeSum(nums, target))
+}
+
--- concurrency_tasks/6_leetcode/hash-table/15_3sum/main.go
+++
@@ -1,3 +0,0 @@
-func threeSum(nums []int) [][]int {
-    
-}
