772cf0a35c0ff95d87738692f9f40e5456359a51
alivewel <alivewel@mail.ru>
2025-02-26 21:48:54 +0300

    .

---
+++ interviews/6_leetcode/sliding_window/overlapping_windows/340_longest-substring-with-at-most-k-distinct-characters/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func longestGeneSequence(gene string, k int) int {
+	
+    return 0
+}
+
+func main() {
+	gene := "YYxxXXXyyy"
+	k := 3
+	fmt.Println(longestGeneSequence(gene, k))
+}
