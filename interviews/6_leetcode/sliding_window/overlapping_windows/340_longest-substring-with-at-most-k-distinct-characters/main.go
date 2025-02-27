fbddd11a73876dd8d2cd8b7c733dd8d93e587ff5
alivewel <alivewel@mail.ru>
2025-02-27 18:11:30 +0300

    .

--- interviews/6_leetcode/sliding_window/overlapping_windows/340_longest-substring-with-at-most-k-distinct-characters/main.go
+++ interviews/6_leetcode/sliding_window/overlapping_windows/340_longest-substring-with-at-most-k-distinct-characters/main.go
@@ -2,31 +2,6 @@ package main
 
 import "fmt"
 
-// func longestGeneSequence(gene string, k int) int {
-// 	countMap := make(map[rune]int, k)
-// 	l, r := 0, 0
-// 	for l < len(gene) {
-// 		// for r < len(gene) && gene[r] == gene[r+1] {
-// 		// 	r++
-// 		// }
-// 		// смотрим наличие в мапе и размер мапы не больше 3
-// 		// for len(countMap) < k || _, found := countMap[ch]; found {
-// 		// 	r++
-// 		// 	countMap[ch]++
-// 		// }
-// 		for len(countMap) < k || _, found := countMap[rune(gene[r])]; found {
-//             countMap[rune(gene[r])]++
-//             r++
-//         }
-// 		l = r + 1
-// 		r = r + 1
-// 	}
-// 	// for _, ch := range gene {
-
-// 	// }
-// 	return 0
-// }
-
 func isMapContaine(m map[rune]int, key rune) bool {
     _, found := m[key]
     return found
@@ -37,19 +12,21 @@ func longestGeneSequence(gene string, k int) int {
     l, r := 0, 0
     maxCount := 0
     for l < len(gene) {
-        for r < len(gene) && (len(countMap) < k || isMapContaine(countMap, rune(gene[r]))) {
-            countMap[rune(gene[r])]++ // тут нужен индекс r + 1, подумать почему
+        for r + 1 < len(gene) && (len(countMap) < k || isMapContaine(countMap, rune(gene[r+1]))) {
+            countMap[rune(gene[r+1])]++ // тут нужен индекс r + 1, подумать почему
             r++
         }
         count := r - l + 1
         maxCount = max(count, maxCount)
+
+        countMap[rune(gene[l])]++
         if countMap[rune(gene[l])] == 0 {
-            delete(countMap, gene[l])
+            delete(countMap, rune(gene[l]))
         }
         
         l = r + 1
     }
-    return 0
+    return maxCount
 }
 
 func main() {
