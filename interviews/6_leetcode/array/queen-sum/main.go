dfd820b57d012517e14b2f62096c68b479538cba
alivewel <alivewel@mail.ru>
2025-02-28 17:47:13 +0300

    .

---
+++ interviews/6_leetcode/array/queen-sum/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func maxQueenSum(board [][]int) int {
+
+	return 0
+}
+
+func main() {
+	board := []int{
+		{3,2,5,1},
+		{4,9,1,3},
+		{2,9,2,1}
+	}
+	fmt.Println(maxQueenSum(board))
+}
