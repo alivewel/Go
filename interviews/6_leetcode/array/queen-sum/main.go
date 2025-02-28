c93d87e6f8bbe4310ca7d49e0b759f238b8f66f2
alivewel <alivewel@mail.ru>
2025-02-28 23:17:59 +0300

    .

--- interviews/6_leetcode/array/queen-sum/main.go
+++ interviews/6_leetcode/array/queen-sum/main.go
@@ -5,15 +5,34 @@ import (
 )
 
 func maxQueenSum(board [][]int) int {
-
-	return 0
+	n := len(board) // количество строк
+	m := len(board[0]) // количество столбцов
+	rows := make([]int, n)
+	cols := make([]int, m)
+	d1Sum := make([]int, n+m-1) // пока не знаю какую длину использовать при создании
+	d2Sum := make([]int, n+m-1) // пока не знаю какую длину использовать при создании
+	maxRes := 0
+	
+	for i := 0; i < len(board); i++ {
+		for j := 0; j < len(board[0]); j++ {
+			rows[i] += board[i][j]
+			cols[j] += board[i][j]
+			d1Sum[i - j + m - 1] += board[i][j]
+			d2Sum[i + j] += board[i][j]
+		}
+	}
+	fmt.Println(rows)
+	fmt.Println(cols)
+	fmt.Println(d1Sum)
+	fmt.Println(d2Sum)
+	return maxRes
 }
 
 func main() {
-	board := []int{
-		{2,2,1,1},
-		{1,2,1,1},
-		{1,2,2,1}
-	}
+	board := [][]int{
+        {2, 2, 1, 1},
+        {1, 2, 1, 1},
+        {1, 2, 2, 1},
+    }
 	fmt.Println(maxQueenSum(board))
 }
