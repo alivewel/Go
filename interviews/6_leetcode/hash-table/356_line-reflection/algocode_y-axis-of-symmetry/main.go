21e143be296b5eecfcf402495bba22f2e9b396a0
alivewel <alivewel@mail.ru>
2025-03-05 20:43:36 +0300

    .

--- interviews/6_leetcode/hash-table/356_line-reflection/algocode_y-axis-of-symmetry/main.go
+++ interviews/6_leetcode/hash-table/356_line-reflection/algocode_y-axis-of-symmetry/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func isSymmetric(points [][]int) bool {
     minX, maxX := 0, 0
     setPoints := make(map[[2]int]struct{})
@@ -15,9 +17,21 @@ func isSymmetric(points [][]int) bool {
         }
         setPoints[[2]int{point[0], point[1]}] = struct{}{}
     }
+    for _, point := range points {
+                   //3 ==  1 + 4 - 2
+                   //4 ==  1 + 4 - 1
+        simPoint := minX + maxX - point[0] // minX + maxX - x
+        if _, found := setPoints[[2]int{simPoint, point[1]}]; !found {
+            return false
+        }
+    }
+    return true
 }
 //   x y
 // [[1,2],[3,1],[4,2],[2,1],[2,1]]
+
+// изначально прочитал readme, чтобы вспомнить алгоритм решения
+// не смог вспомнить формулу для нахождения симметричной точки
 
 func main() {
 	points := [][]int{{1,2},{3,1},{4,2},{2,1},{2,1}}
