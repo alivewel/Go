36fc546f24cb4b622d8b38d1db0b2d347767cad9
vasilev.a178 <vasilev.a178@mail.ru>
2025-03-05 16:48:53 +0300

    .

--- interviews/6_leetcode/hash-table/332_reconstruct-itinerary/algocode_build-a-route/main.go
+++ interviews/6_leetcode/hash-table/332_reconstruct-itinerary/algocode_build-a-route/main.go
@@ -1,5 +1,37 @@
 package main
 
-func route(tickets [][]string) []string {
+import "fmt"
 
+func route(tickets [][]string) []string {
+	dist := make(map[string]struct{})
+	mapping := make(map[string]string)
+	res := make([]string, 0)
+	for _, ticket := range tickets {
+		dist[ticket[1]] = struct{}{}
+		mapping[ticket[0]] = ticket[1]
+	}
+	for _, ticket := range tickets { // ищем первый город
+		if _, found := dist[ticket[0]]; !found {
+			res = append(res, ticket[0])
+			break
+		}
+	}
+	var src string
+	if len(res) > 0 {
+		src = res[0]
+	}
+	for {
+		if dist, found := mapping[src]; found {
+			res = append(res, dist)
+			src = dist
+		} else {
+			break
+		}
+	}
+	return res
+}
+
+func main() {
+	tickets := [][]string{{"B", "C"},{"A", "B"},{"C", "D"}}
+	fmt.Println(route(tickets))
 }
