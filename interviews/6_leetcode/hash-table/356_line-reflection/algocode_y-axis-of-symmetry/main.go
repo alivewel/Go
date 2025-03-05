9274991671ba8d2d93dc420d3882fa4f9df60b6b
alivewel <alivewel@mail.ru>
2025-03-05 20:07:38 +0300

    .

---
+++ interviews/6_leetcode/hash-table/356_line-reflection/algocode_y-axis-of-symmetry/main.go
@@ -0,0 +1,11 @@
+package main
+
+func isSymmetric(points [][]int) bool {
+	
+	return true
+}
+
+func main() {
+	points := [][]int{{1,2},{3,1},{4,2},{2,1},{2,1}}
+	fmt.Println(isSymmetric(points))
+}
