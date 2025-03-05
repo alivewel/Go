73406dd44940ef280ce472a04945f177e6279473
alivewel <alivewel@mail.ru>
2025-03-05 20:23:05 +0300

    .

--- interviews/6_leetcode/hash-table/356_line-reflection/algocode_y-axis-of-symmetry/main.go
+++ interviews/6_leetcode/hash-table/356_line-reflection/algocode_y-axis-of-symmetry/main.go
@@ -1,9 +1,23 @@
 package main
 
 func isSymmetric(points [][]int) bool {
-	
-	return true
+    minX, maxX := 0, 0
+    setPoints := make(map[[2]int]struct{})
+    for i, point := range points {
+        if i == 0 {
+            minX, maxX = point[0], point[0]
+        }
+        if minX > point[0] {
+            minX = point[0]
+        }
+        if maxX < point[0] {
+            maxX = point[0]
+        }
+        setPoints[[2]int{point[0], point[1]}] = struct{}{}
+    }
 }
+//   x y
+// [[1,2],[3,1],[4,2],[2,1],[2,1]]
 
 func main() {
 	points := [][]int{{1,2},{3,1},{4,2},{2,1},{2,1}}
