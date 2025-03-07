31e3b18e145114e0c3deecfbccb9dd7ca2d88abe
vasilev.a178 <vasilev.a178@mail.ru>
2025-03-07 12:24:23 +0300

    227_basic-calculator-ii

--- interviews/6_leetcode/array/227_basic-calculator-ii/1/main.go
+++ interviews/6_leetcode/array/227_basic-calculator-ii/1/main.go
@@ -14,7 +14,12 @@
 
 // попробую сначала решить через очередь
 package main
-                 3
+
+import (
+    "fmt"
+    "strconv"
+)
+                //  3
 // s = ["1","+","2","+","3","*","4"]
 // queueNums 3 12
 // queueOper + 
@@ -29,7 +34,7 @@ func calculate(s []string) int {
             stackNums.Push(str)
         }
         if isOper(str) {
-            if str == "+" {
+            if str == "+" || str == "*" {
               if stackOper.Size > 0 && stackNums.Size > 1 {
                   num1, num2 := stackNums.Pop(), stackNums.Pop()
                   oper := stackOper.Pop()
@@ -40,18 +45,61 @@ func calculate(s []string) int {
                       res := strconv.Atoi(num1) * strconv.Atoi(num2)
                       stackNums.Push(strconv.Itoa(res))
                   }
-              } else {
-                stackOper.Push(str)  
               }
-              
-            }
-            if str == "*" {
-                
+              stackOper.Push(str)  
             }
         }
     }
-
+    if stackOper.Size > 0 && stackNums.Size > 1 {
+      num1, num2 := stackNums.Pop(), stackNums.Pop()
+      oper := stackOper.Pop()
+      if oper == "+" {
+          res := strconv.Atoi(num1) + strconv.Atoi(num2)
+          stackNums.Push(strconv.Itoa(res))
+      } else if oper == "*" {
+          res := strconv.Atoi(num1) * strconv.Atoi(num2)
+          stackNums.Push(strconv.Itoa(res))
+      }
+    }
+    return stackNums.Pop()
 }
+
+// Структура для стека
+type Stack struct {
+    elements []string // Используем string для хранения символов
+}
+
+// Добавление символа в стек
+func (s *Stack) Push(value string) {
+    s.elements = append(s.elements, value)
+}
+
+// Удаление символа из стека
+func (s *Stack) Pop() (string, bool) {
+    if len(s.elements) == 0 {
+        return 0, false // Если стек пуст
+    }
+    lastIndex := len(s.elements) - 1
+    value := s.elements[lastIndex]
+    s.elements = s.elements[:lastIndex]
+    return value, true
+}
+
+// Проверка, пуст ли стек
+func (s *Stack) IsEmpty() bool {
+    return len(s.elements) == 0
+}
+
+// Получение количества элементов в стеке
+func (s *Stack) Size() int {
+    return len(s.elements)
+}
+
+func main() {
+    s := []string{"2","*","3","*","1","+","2","+","2","+","2","*","0","*","13","+","1"}
+	fmt.Println(calculate(s))
+}
+
 
 // strconv.Itoa(num) - конвертация числа в строку
 // strconv.Atoi(str) - конвертация строки в число
