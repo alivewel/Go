daef35804e2dc2bc4bfc79135e661c7fba80d217
alivewel <alivewel@mail.ru>
2025-03-12 23:06:23 +0300

    567_permutation-in-string

--- interviews/6_leetcode/sliding_window/overlapping_windows/567_permutation-in-string/leetcode/main.go
+++ interviews/6_leetcode/sliding_window/overlapping_windows/567_permutation-in-string/leetcode/main.go
@@ -25,25 +25,25 @@ func containsMap(mapVirus map[byte]int, ch byte) bool {
 
 func checkInclusion(s1, s2 string) bool {
     mapVirus := make(map[byte]int)
-    for i := range s2 {
-        mapVirus[s2[i]]++
+    for i := range s1 {
+        mapVirus[s1[i]]++
     }
     l, r := 0, -1
     window := make(map[byte]int)
-    for l < len(s1) {
+    for l < len(s2) {
         // при формировании окна смотреть есть ли элемент в mapVirus
-        for r + 1 < len(s1) && r - l + 1 != len(s2) && containsMap(mapVirus, s1[r+1]) {
+        for r + 1 < len(s2) && r - l + 1 != len(s1) && containsMap(mapVirus, s2[r+1]) {
             r++ 
-            window[s1[r]]++
+            window[s2[r]]++
         }
         sizeWindow := r - l + 1
-        if sizeWindow == len(s2) && checkEqualMap(mapVirus, window) {
+        if sizeWindow == len(s1) && checkEqualMap(mapVirus, window) {
             return true
         }
-        window[s1[l]]--
-		// если элементов window[s1[l]] == 0, тоже нужно удалять элементы из окна скорее всего
-        if val, _ := window[s1[l]]; val == -1 {
-            delete(window, s1[l])
+        window[s2[l]]--
+		// если элементов window[s2[l]] == 0, тоже нужно удалять элементы из окна скорее всего
+        if val, _ := window[s2[l]]; val == -1 {
+            delete(window, s2[l])
             r++
         }
         l++
@@ -52,7 +52,9 @@ func checkInclusion(s1, s2 string) bool {
 }
 
 func main() {
-	s1 := "cdeebba"
-	s2 := "abb"
+	// s2 := "cdeebba"
+	// s1 := "abb"
+    s1 := "ab"
+	s2 := "eidbaooo"
 	fmt.Println(checkInclusion(s1, s2))
 }
