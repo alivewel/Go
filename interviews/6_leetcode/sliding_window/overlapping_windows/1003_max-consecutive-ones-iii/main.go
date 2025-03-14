e3af97f8c0d0ca3f79f092217460efdbfcd6b904
alivewel <alivewel@mail.ru>
2025-03-14 23:23:07 +0300

    1003_max-consecutive-ones-iii

---
+++ interviews/6_leetcode/sliding_window/overlapping_windows/1003_max-consecutive-ones-iii/main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func longestOnes(nums []int, k int) int {
+    l, r := 0, -1
+    maxCount := 0
+    countZero := 0
+    for l < len(nums) {
+		// fmt.Println(l, r)
+        for r + 1 < len(nums) && (countZero < k || nums[r+1] == 1) {
+            if nums[r+1] == 0 {
+                countZero++
+            }
+            r++
+        }
+		windowSize := r - l + 1
+        maxCount = max(windowSize, maxCount) //  // 
+        fmt.Println(windowSize, maxCount)
+		if nums[l] == 0 {
+            countZero--
+        }
+        l++
+    }
+    return maxCount
+}
+
+func main() {
+	nums := []int{1,1,1,0,0,0,1,1,1,1,0}
+	k := 2
+	fmt.Println(longestOnes(nums, k)) // Expected 6
+}
