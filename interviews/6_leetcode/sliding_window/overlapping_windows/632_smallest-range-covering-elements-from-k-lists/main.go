6723d553e255674c9d09232038cef16f4f0c891b
alex_go <alex_go@mail.ru>
2025-04-28 19:45:25 +0300

    632_smallest-range-covering-elements-from-k-lists

--- interviews/6_leetcode/sliding_window/overlapping_windows/632_smallest-range-covering-elements-from-k-lists/main.go
+++ interviews/6_leetcode/sliding_window/overlapping_windows/632_smallest-range-covering-elements-from-k-lists/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "sort"
+import (
+	"fmt"
+	"math"
+	"sort"
+)
 
 type Element struct {
 	Idx int
@@ -13,25 +17,25 @@ func smallestRange(nums [][]int) []int {
 	elements := []Element{}
 	for i, curNums := range nums {
 		for _, num := range curNums {
-			elements = append(elements, Element{Idx: i, Val: n})
+			elements = append(elements, Element{Idx: i, Val: num})
 		}
 	}
 
-	sort.Slices(elements, func(i, j int) bool {
+	sort.Slice(elements, func(i, j int) bool { // sort.Slices
 		return elements[i].Val < elements[j].Val
 	})
 
 	left := 0
 	count := make(map[int]int, 0)
 	res := make([]int, 0)
-	bestRange := 0
+	bestRange := math.MaxInt // bestRange := 0
 	for right := range elements {
 		rightElem := elements[right]
 		count[rightElem.Idx]++
 		for len(nums) == len(count) {
 			leftElem := elements[left]
 			curRange := rightElem.Val - leftElem.Val
-			if bestRange > curRange {
+			if bestRange > curRange { //  || (bestRange == curRange && leftElem.Val < res[0])
 				bestRange = curRange
 				res = res[:0]
 				res = append(res, leftElem.Val, rightElem.Val)
@@ -44,4 +48,15 @@ func smallestRange(nums [][]int) []int {
 		}
 	}
     return res
+}
+
+func main() {
+	nums := [][]int{
+		{4, 10, 15, 24, 26},
+		{0, 9, 12, 20},
+		{5, 18, 22, 30},
+	}
+
+	result := smallestRange(nums)
+	fmt.Println(result) // Ожидается: [20 24]
 }
